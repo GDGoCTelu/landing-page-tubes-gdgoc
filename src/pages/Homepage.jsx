15e0ddd43a64a91eb27c2a7bba6ebb23be27f2b8
Reynadi Andreas Priesley <rey@reynadi.com>
2025-04-05 22:15:58 +0700

    feat: done with exclusive deals section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import Navbar from '../components/Section/Navbar';
 import FeatureCard from '../components/Card/FeatureCard';
 import TodoList from '../constants/ToDoList';
+import DealsList from '../constants/DealsList';
+import DealCard from '../components/Card/DealCard';
 
 function Homepage() {
   return (
@@ -47,6 +49,37 @@ function Homepage() {
           {TodoList.map((item, index) => (
             <FeatureCard key={index} {...item} />
           ))}
+        </div>
+      </section>
+      <section className='bg-background-white text-primary-black font-body h-[960px] w-full'>
+        <h2 className='font-display text-h2 pt-[120px] text-center font-bold'>
+          Exclusive{' '}
+          <span className='text-primary-orange'>deals & discounts</span>
+        </h2>
+        <p className='text-p2 text-light-gray mx-auto mt-4 w-[433px] text-center leading-[32px]'>
+          Discover our fantastic early booking discounts & start planning your
+          journey.
+        </p>
+        <div className='mx-auto mt-8 grid w-[1170px] grid-cols-4'>
+          {DealsList.map((item, index) => (
+            <DealCard key={index} {...item} />
+          ))}
+        </div>
+        <div className='mt-[71px] flex flex-row items-center justify-center gap-3'>
+          <button className='bg-background-white h-[40px] w-[40px] rounded-full border-[1px] border-[#999999]/50'>
+            <img
+              className='mx-auto my-auto'
+              src='/assets/arrow-gray.svg'
+              alt='Arrow Left'
+            />
+          </button>
+          <button className='bg-primary-orange h-[40px] w-[40px] rounded-full'>
+            <img
+              className='mx-auto my-auto'
+              src='/assets/arrow-white.svg'
+              alt='Arrow Right'
+            />
+          </button>
         </div>
       </section>
     </>
